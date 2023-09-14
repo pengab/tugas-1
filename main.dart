8ef6f04e5e88e39009d78bd6fa44361494e9c091
ngabiel <136612342+pengab@users.noreply.github.com>
2023-09-14 13:14:02 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,11 +5,17 @@ class Mahasiswa {
 
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
+  int umur() {
+    int tahun = DateTime.now().year;
+    return tahun - this.tahunLahir;
+  }
+
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya ${umur()} tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021095", "M Irfan Nabiel M Mucoffa", 2004);
+  mhs.perkenalan();
 }
